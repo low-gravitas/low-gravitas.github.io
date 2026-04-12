93a0289807a8da026fdf191b0138cc04a66fd8a4
Mike Abney <git@mikeabney.com>
2026-04-12 17:05:26 -0400

    Remove unpublished LLM post; let draft pages render at their URLs

--- src/blog/blog.11tydata.js
+++ src/blog/blog.11tydata.js
@@ -1,12 +1,5 @@
-const isProduction = process.env.NODE_ENV === "production";
-
 export default {
   layout: "layouts/post.njk",
   tags: ["blog"],
   extraCss: "/css/blog.css",
-  permalink: (data) => {
-    // Draft posts excluded from production builds
-    if (isProduction && data.draft) return false;
-    return undefined; // use default permalink
-  },
 };
